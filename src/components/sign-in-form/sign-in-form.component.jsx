1d7948f3e9f16282812c70b47683a1eaec52d196
Chamindu <cudakara@gmail.com>
2023-07-11 14:24:01 +0530

    Finishing up Authentication page

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -41,12 +41,18 @@ const SignInForm = () => {
 
             resetFormFields();
         } catch (error) {
-            console.error('User creation on Firebase encountered an error: ', error);
+            if (error.code === 'auth/wrong-password') {
+                alert('Incorrect password');
+            } else if (error.code === 'auth/user-not-found') {
+                alert('User not found');
+            } else {
+                console.error('User sign in encountered an error: ', error);
+            };
         };
     };
 
     return (
-        <div>
+        <div className='sign-in-container'>
             <h2>Already have an account?</h2>
             <span>Sign in with your email and password</span>
             <form onSubmit={handleSubmit}>
@@ -66,14 +72,14 @@ const SignInForm = () => {
                     name="password"
                     value={password} />
 
-                <div className='buttons-container'>
+                <div className="buttons-container">
                     <Button type='submit'>Sign In</Button>
                     <Button
                         buttonType='google'
                         type='button'
                         onClick={signInWithGoogle}
                     >
-                        Sign In With Google
+                        Google Sign In
                     </Button>
                 </div>
             </form>
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,31 +1,15 @@
-import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
-import {
-    signInWithGooglePopup,
-    signInWithGoogleRedirect,
-    createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+
+import "./authentication.styles.scss";
 
 const Authentication = () => {
-    const logGoogleUser = async () => {
-        // sign in with google popup
-        const { user } = await signInWithGooglePopup();
-        // create the user upon sign in
-        await createUserDocumentFromAuth(user);
-    };
-
     return (
-        <div>
-            <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>
-                Sign in with Google Popup
-            </button>
-            <button onClick={signInWithGoogleRedirect}>
-                Sign in with Google Redirect
-            </button>
+        <div className="authentication-container">
             <SignInForm />
             <SignUpForm />
         </div>
     );
-}
+};
+
 export default Authentication;
