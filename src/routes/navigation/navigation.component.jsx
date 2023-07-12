1cc9b583197d54efdc0e7264f8c48190f3bf7849
Chamindu <cudakara@gmail.com>
2023-07-12 16:01:29 +0530

    Add cart context and implement cart dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,10 +5,14 @@ import { ReactComponent as HomeLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
 import { UserContext } from '../../contexts/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+import { CartContext } from '../../contexts/cart.context';
 
 const Navigation = () => {
 
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext)
 
     const signOutHandler = async () => {
         await signOutUser();
@@ -31,7 +35,9 @@ const Navigation = () => {
                             SIGN-IN
                         </Link>
                     }
+                    <CartIcon />
                 </div>
+                {isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </Fragment>
