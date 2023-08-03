3a6262b65ab2862cc93442e07ad24d2f6bdcfbba
Chamindu <cudakara@gmail.com>
2023-08-03 22:54:53 +0530

    Add redux-async libraries to handle fetching of categories

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,13 @@
+// parserOptions: {
+//     parser: '@babel/eslint-parser',
+//         requireConfigFile: false, // <== ADD THIS
+//             ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+//                 sourceType: 'module' // Allows for the use of imports
+// }
+
 parserOptions: {
-    parser: '@babel/eslint-parser',
-        requireConfigFile: false, // <== ADD THIS
-            ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-                sourceType: 'module' // Allows for the use of imports
+    parser: '@babel/eslint-parser';
+    requireConfigFile: false; // <== ADD THIS
+    ecmaVersion: 2018; // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module' // Allows for the use of imports
 }
