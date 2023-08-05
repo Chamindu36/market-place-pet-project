d66db36aed372678e0a37dbf37c8ce00b0e4a172
Chamindu <cudakara@gmail.com>
2023-08-05 19:09:17 +0530

    Add signUp and signOut sagas to user reducers using redux saga.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,23 +1,24 @@
 import { Outlet } from 'react-router-dom';
 import { Fragment } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { ReactComponent as HomeLogo } from '../../assets/crown.svg';
 import { LogoContainer, NavLink, NavLinks, NavigationContainer } from './navigation.styles.jsx';
-import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { selectCurrentUser } from '../../store/user/user.selector'
 import { selectIsCartOpen } from '../../store/cart/cart.selector';
 
+import { signOutStart } from '../../store/user/user.action';
+
 const Navigation = () => {
+    const dispatch = useDispatch();
+
     const currentUser = useSelector(selectCurrentUser);
-    console.log("((((((((", currentUser);
     const isCartOpen = useSelector(selectIsCartOpen);
 
-    const signOutHandler = async () => {
-        await signOutUser();
-    }
+    const signOutUser = () => dispatch(signOutStart());
+
 
     return (
         <Fragment>
@@ -31,7 +32,7 @@ const Navigation = () => {
                     </NavLink>
                     {
                         currentUser ? (
-                            <NavLink as='span' onClick={signOutHandler}>SIGN OUT</NavLink>
+                            <NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>
                         ) : <NavLink to="/auth">
                             SIGN-IN
                         </NavLink>
--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -5,6 +5,10 @@ import { USER_ACTION_TYPES } from './user.types';
 import {
     signInSuccess,
     signInFailed,
+    signUpSuccess,
+    signUpFailed,
+    signOutSuccess,
+    signOutFailed,
 } from './user.action';
 
 import {
@@ -12,16 +16,19 @@ import {
     createUserDocumentFromAuth,
     signInWithGooglePopup,
     signInAuthUserWithEmailAndPassword,
+    createAuthUserWithEmailAndPassword,
+    signOutUser,
 } from '../../utils/firebase/firebase.utils';
 
-
 export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
+    console.log('getSnapshotFromUserAuth', userAuth, '\ntttt::', additionalDetails);
     try {
         const userSnapshot = yield call(
             createUserDocumentFromAuth,
             userAuth,
             additionalDetails
         );
+        console.log('userSnapshot', userSnapshot);
         yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
     } catch (error) {
         yield put(signInFailed(error));
@@ -35,12 +42,15 @@ export function* signInWithGoogle() {
     } catch (error) {
         yield put(signInFailed(error));
     }
+}
 
-};
-
-export function* signInWithEmailAndPassword({ payload: { email, password } }) {
+export function* signInWithEmail({ payload: { email, password } }) {
     try {
-        const { user } = yield call(signInAuthUserWithEmailAndPassword, email, password);
+        const { user } = yield call(
+            signInAuthUserWithEmailAndPassword,
+            email,
+            password
+        );
         yield call(getSnapshotFromUserAuth, user);
     } catch (error) {
         yield put(signInFailed(error));
@@ -50,25 +60,61 @@ export function* signInWithEmailAndPassword({ payload: { email, password } }) {
 export function* isUserAuthenticated() {
     try {
         const userAuth = yield call(getCurrentUser);
-        console.log('isUserAuthenticated', userAuth);
         if (!userAuth) return;
         yield call(getSnapshotFromUserAuth, userAuth);
     } catch (error) {
         yield put(signInFailed(error));
     }
-};
+}
 
-export function* onCheckUserSession() {
-    console.log("onCheckUserSession");
-    yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
-};
+export function* signUp({ payload: { email, password, displayName } }) {
+    try {
+        const { user } = yield call(
+            createAuthUserWithEmailAndPassword,
+            email,
+            password
+        );
+        yield put(signUpSuccess(user, { displayName }));
+    } catch (error) {
+        yield put(signUpFailed(error));
+    }
+}
+
+export function* signOut() {
+    try {
+        yield call(signOutUser);
+        yield put(signOutSuccess());
+    } catch (error) {
+        yield put(signOutFailed(error));
+    }
+}
+
+export function* signInAfterSignUp({ payload: { user, additionalDetails } }) {
+    yield call(getSnapshotFromUserAuth, user, additionalDetails);
+}
 
 export function* onGoogleSignInStart() {
     yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
 
+export function* onCheckUserSession() {
+    yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
 export function* onEmailSignInStart() {
-    yield takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmailAndPassword);
+    yield takeLatest(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, signInWithEmail);
+}
+
+export function* onSignUpStart() {
+    yield takeLatest(USER_ACTION_TYPES.SIGN_UP_START, signUp);
+}
+
+export function* onSignUpSuccess() {
+    yield takeLatest(USER_ACTION_TYPES.SIGN_UP_SUCCESS, signInAfterSignUp);
+}
+
+export function* onSignOutStart() {
+    yield takeLatest(USER_ACTION_TYPES.SIGN_OUT_START, signOut);
 }
 
 export function* userSagas() {
@@ -76,5 +122,8 @@ export function* userSagas() {
         call(onCheckUserSession),
         call(onGoogleSignInStart),
         call(onEmailSignInStart),
+        call(onSignUpStart),
+        call(onSignUpSuccess),
+        call(onSignOutStart),
     ]);
-};
+}
