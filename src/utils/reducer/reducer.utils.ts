f8936d76b4a0501e5de63c30c984445419f3f2a5
Chamindu <cudakara@gmail.com>
2023-08-17 23:24:45 +0530

    Define matchable for Actions for Typescript/Redux

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -1,5 +1,27 @@
 import { AnyAction } from "redux";
  
+// match actions directly to the types
+type Matchable <AC extends () => AnyAction> = AC & {
+    type: ReturnType<AC>['type']; // get the return type of the action
+    match(action : AnyAction): action is ReturnType<AC>;
+};
+
+// define withMatch function for overloading actions
+export function withMatcher<AC extends () => AnyAction & { type: string }>(actionCraetor:AC) : Matchable<AC>;
+
+export function withMatcher<AC extends (...args: any[]) => AnyAction & { type: string }>(actionCraetor:AC) : Matchable<AC>;
+
+export function withMatcher (actionCreator: Function) {
+    const type = actionCreator().type; // get the type of the action for every action 
+
+    return Object.assign( actionCreator, {
+        type,
+        match (action: AnyAction) {
+            return action.type === type;
+        }
+    });
+};
+
 export type ActionWithPayload <T,P> = {
     type: T;
     payload: P;
